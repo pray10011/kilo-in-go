4797d5bfa1c6d077877981b2a14ce5ad8fb0df37
wpj <837289221@qq.com>
2025-03-25 17:00:51 +0800

    step 5

--- main.go
+++ main.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"fmt"
+	"syscall"
+	"unsafe"
+
 	"io"
 	"os"
 )
 
+func enableRawMode() {
+	// 获取当前终端属性
+	fd := int(os.Stdin.Fd())
+	var oldTermios,newTermios syscall.Termios
+	_,_ ,errno  := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TCGETS), uintptr(unsafe.Pointer(&oldTermios)))
+	if errno != 0 {
+		fmt.Println("get termios error:", errno)
+		return
+	}
+
+	newTermios = oldTermios
+
+	newTermios.Lflag &= ^(uint32(syscall.ECHO))
+
+	// 写入新的终端属性
+	_,_ ,errno  = syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(&newTermios)))
+	if errno != 0 {
+		fmt.Println("set termios error:", errno)
+		return
+	}
+}
+
 func main() {
+	enableRawMode()
 	var c []byte = make([]byte, 1)
 	for {
 		n, err := os.Stdin.Read(c)
